59e0dac85bc0c15b66fcaadfcd33acf9f0550093
Md Abdur Rahman Palash <abdurrahmanpalashbd@gmail.com>
2026-06-28 18:55:34 +0600

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,14 +7,4 @@
   </div>
 </template>
 
-<script setup>
-useHead({
-  script: [
-    {
-      src: "https://aics-sdcf.onrender.com/js/chat-widget.js?v=1781715266965",
-      "data-business-id": "0cb9f74afaa31ddf836d22666eb892d9",
-      body: true,
-    },
-  ],
-})
-</script>
+
